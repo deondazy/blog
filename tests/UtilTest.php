efc621776e1fae93fe0386b15ea8d21d7d9f8725
deondazy <deondazy@gmail.com>
2022-09-22 16:37:59 +0100

    Add testing with PHPUnit

---
+++ tests/UtilTest.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+final class UtilTest extends TestCase
+{
+    public function testPushAndPop()
+    {
+        // Do test stuff
+    }
+}
